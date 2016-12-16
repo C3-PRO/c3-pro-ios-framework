250d3441f73b1a297ca0a57930f23bf8d5582cf4
Pascal Pfiffner <phase.of.matter@gmail.com>
2016-12-16 16:08:56 +0100

    Care for localization, add German

---
+++ extract_localizable_strings.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+#
+#  Find all localizable strings, used in code as indicated by PATTERN
+
+import io
+import re
+import sys
+import glob
+
+# the 1st group must capture the actual string
+#PATTERN = re.compile(r'"((([^"])|(\\"))+?)".c3_localized')
+# TODO: how to zip past '\"'?
+PATTERN = re.compile(r'"([^"]+?)".c3_localized')
+# TODO: extract the comments if this form is used: .c3_localized("Start Consent button title")
+
+REFERENCE_FILE = 'en.lproj/C3PRO.strings'
+
+def parse_into(filepath, into):
+	with open(filepath, 'r') as handle:
+		text = handle.read()
+		for res in re.finditer(PATTERN, text):
+			if res is not None:
+				into.add(res.group(1))
+
+
+if '__main__' == __name__:
+	found = set()
+	
+	# tests
+	if False:
+		tests = [
+			'let str = "1 Hello".c3_localized',
+			'let str = "2 Hello (World)".c3_localized',
+			'let str = "3 Hello \"World\"".c3_localized',
+			'let str = "The \("4 Hello".c3_localized) and \("5 World".c3_localized)"'
+			'let bar = "Hello"\nfunction(is: true)\nlet two = "6 World".c3_localized'
+		]
+		for test in tests:
+			for res in re.finditer(PATTERN, test):
+				print(res.group(1) if res else None)
+		sys.exit(0)
+	
+	# loop all implementation files
+	for filepath in glob.glob("Sources/*/*.swift"):
+		parse_into(filepath, found)
+	
+	# look at Localizable.strings
+	existing = {}
+	with io.open(REFERENCE_FILE, 'r', encoding='utf-8') as handle:
+		for line in handle:
+			line = line.strip()
+			if len(line) > 0:
+				parts = line.split('" = "')
+				if 2 == len(parts):
+					key = parts[0][1:]
+					existing[key] = line
+	
+	# show all, alphabetically ordered
+	for string in sorted(found, key=lambda s: s.lower()):
+		if string in existing:
+			print(existing[string])
+		else:
+			print('"{}" = "{}";'.format(string, string))
+
